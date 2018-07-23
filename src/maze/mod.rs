be7fd4cf7ececf3ea84e3f89e8e235fedba9e577
Marc Breitung <info@marcbreitung.de>
2018-07-23 18:43:25 +0200

    added method add_tile to maze

--- src/maze/mod.rs
+++ src/maze/mod.rs
@@ -1,23 +1,46 @@
-use ::tile;
+use tile::Tile;
 
 pub struct Maze<'a> {
-    pub tiles: Vec<tile::Tile<'a>>
+    pub tiles: Vec<Tile<'a>>
 }
 
 impl<'a> Maze<'a> {
+    // Returns a maze
     #[allow(dead_code)]
     pub fn new() -> Self {
         Maze { tiles: Vec::new() }
+    }
+
+    // Adds a tile to the maze
+    #[allow(dead_code)]
+    pub fn add_tile(&mut self, tile: Tile<'a>) {
+        if !self.tiles.iter().any(|t| t.position == tile.position) {
+            self.tiles.push(tile);
+        }
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use super::Maze;
+    use ::maze::Maze;
+    use ::tile::Tile;
 
     #[test]
     fn maze_new() {
         let maze = Maze::new();
         assert_eq!(0, maze.tiles.len());
+    }
+
+    #[test]
+    fn maze_add_tile() {
+        let mut maze = Maze::new();
+        assert_eq!(0, maze.tiles.len());
+
+        let tile_a = Tile::new();
+        let tile_b = Tile::new();
+        maze.add_tile(tile_a);
+        maze.add_tile(tile_b);
+
+        assert_eq!(1, maze.tiles.len());
     }
 }
